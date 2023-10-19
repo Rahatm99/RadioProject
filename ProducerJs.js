38f371a5267895ad6858c759f0871d27b54aefb0
Rahat Mustafa <Rahatmustafa99@gmail.com>
2023-10-19 01:52:30 -0400

    Updated js file

--- ProducerJs.js
+++ ProducerJs.js
@@ -2,13 +2,6 @@ function confirmBooking() {
     const confirmation = window.confirm("Are you sure you want to book this DJ?")
 }
 
-const element = document.getElementById("songButton");
-element.addEventListener("click", playSong);
-
-function playSong() {
-    alert("Now Playing:");
-}
-
 //Search Function for DJs
 function searchDJs() {
     var input = document.getElementById("searchInput").value.toLowerCase();
@@ -42,4 +35,60 @@ function toggleGridStyle() {
         djList.style.display = "block";
         songList.style.display = "none";
     }
+}
+
+//List of songs
+const songs = [
+    {
+        title: "Bohemian Rhapsody",
+        artist: "Queen",
+        length: "5:55"
+    },
+    {
+        title: "Imagine",
+        artist: "John Lennon",
+        length: "3:02"
+    },
+    {
+        title: "Hotel California",
+        artist: "Eagles",
+        length: "6:30"
+    },
+    {
+        title: "Like a Rolling Stone",
+        artist: "Bob Dylan",
+        length: "6:13"
+    },
+    {
+        title: "Gucci Gang",
+        artist: "Lil Pump",
+        length: "2:11"
+    }
+];
+
+//Creates a list by using the object
+function displaySongs() {
+    const songList = document.getElementById("songList");
+
+    songs.forEach(song => {
+        const list = document.createElement("li");
+        list.textContent = `${song.title} by ${song.artist} (Length: ${song.length})`;
+        
+        const playButton = document.createElement("button");
+        playButton.textContent = "Play";
+        playButton.addEventListener("click", () => {
+            playSong(song.title);
+        });
+
+        list.appendChild(playButton);
+        songList.appendChild(list);
+    });
+}
+
+//Applies after page is loaded
+document.addEventListener("DOMContentLoaded", displaySongs);
+
+//Alert when play button is clicked
+function playSong(songName) {
+    alert(`Now Playing: ${songName}`);
 }
